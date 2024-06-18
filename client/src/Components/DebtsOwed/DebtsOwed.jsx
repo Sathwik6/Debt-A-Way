f15b11afa45ab7b5723111be134ac01d1079d6aa
Anirudh Nemmani <96967353+Ani2nem@users.noreply.github.com>
2024-06-18 10:25:37 -0500

    fixed some merge conflicts

--- client/src/Components/DebtsOwed/DebtsOwed.jsx
+++ client/src/Components/DebtsOwed/DebtsOwed.jsx
@@ -1,8 +1,7 @@
 import axios from "axios"
 import React, { useEffect } from "react"
-client/src/Components/DebtsOwed/DebtsOwed.jsx
-import Debts from "../../Components/Debts"
-import Navbar from "../../Components/Navbar"
+import Debts from "../Debts/Debts"
+import Navbar from "../Navbar/Navbar"
 import { useNavigate } from "react-router-dom"
 import './DebtsOwed.css'
 
